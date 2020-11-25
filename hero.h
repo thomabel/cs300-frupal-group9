9bd641cef6e804e723bf7b3eb53becd91a3a7993
jbrower1215 <74207544+jbrower1215@users.noreply.github.com>
2020-11-24 17:47:51 -0800

    added inventory and destructor stuff

--- hero.h
+++ hero.h
@@ -5,26 +5,30 @@ November 2020
 #include <vector>
 using namespace std;
 
-
 class hero
 {
     public:
     hero();
     hero(const  hero &);
+    ~hero();
     
 
     vector<tool*> getUsableTools(obstacle);
+    void addInventory(tool *);
     bool consumeTool(tool *);
     int visionRange(void);
     bool hasShip(void);
+    int whiffles(void);
+    int energy(void);
+    int addEnergy(int);
+    int addWhiffles(int);
 
     private:
     vector<tool*> inventory_;
     bool hasBinoculars_;
     bool hasShip_;
-
-
+    int whiffles_;
+    int energy_;
 
 
 };
-
