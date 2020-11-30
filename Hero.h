71fe23ec33c7771225ae09c276c51c1ac26afb5c
jbrower1215 <74207544+jbrower1215@users.noreply.github.com>
2020-11-29 17:01:41 -0800

    Update and rename hero.h to Hero.h

--- Hero.h
+++ Hero.h
@@ -5,16 +5,7 @@ November 2020
 #include <vector>
 using namespace std;
 
-
-struct tool{
-    int num = 0;
-};
-
-struct obstacle{
-    int num = 0;
-};
-
-class hero{
+class Hero{
     public:
     hero();
     hero(const  hero &);
